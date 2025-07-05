d94c0e27bc555f00946fbb980cb21521cc1f7a21
brandishcode <brandishcode@gmail.com>
2025-07-05 17:13:36 +0900

    have the caller of Process#exec to handle stdout, stderr, an on_exit callbacks

--- lua/nixessitycore.lua
+++ lua/nixessitycore.lua
@@ -1,21 +1,42 @@
-local function flake_output(flake_path)
+local json = require 'cjson'
+
+local function abs_path(path)
+  local output = {}
   local process = require 'nixessitycore.process'
-  local abs_path = process({ cmd = 'readlink', args = { '-f', flake_path }, to_string = true })
-  return process({
+  process({
+    cmd = 'readlink',
+    args = { '-f', path },
+    listeners = {
+      on_stdout = function(_, data)
+        table.insert(output, data)
+      end,
+    },
+  })
+  return string.gsub(table.concat(output), '%s+', '')
+end
+
+local function flake_output(flake_path)
+  local output = {}
+  local process = require 'nixessitycore.process'
+  process({
     cmd = 'nix',
     args = {
       'eval',
       '--expr',
       string.format(
         'builtins.attrNames (builtins.getFlake "%s").outputs.packages.${builtins.currentSystem}',
-        abs_path
+        abs_path(flake_path)
       ),
       '--impure',
       '--json',
     },
-    to_json = true,
-    inherit_stderr = true,
+    listeners = {
+      on_stdout = function(_, data)
+        table.insert(output, data)
+      end,
+    },
   })
+  return json.decode(table.concat(output))
 end
 
 return setmetatable({ flake_output = flake_output }, {})
--- lua/nixessitycore/process.lua
+++ lua/nixessitycore/process.lua
@@ -1,87 +1,88 @@
-local json = require 'cjson'
+---@alias ListenerType
+---| "parent" # inherits the parent stdout or stderr
+
+---@alias ListenerCallback ListenerType|fun(err: string, data: string)
+
+---@class Listeners
+---@field on_stdout? ListenerCallback by default 'parent'
+---@field on_stderr? ListenerCallback by default 'parent'
+---@field on_exit? fun(err: string, data: string)
 
 ---@class SpawnOpts
 ---@field cmd string the executable
 ---@field args table the executable arguments
----@field inherit_stderr boolean to inherit the parent stderr
----@field inherit_stdout boolean to inherit the parent stdout
+---@field listeners? Listeners
 
 ---Execute a process
 ---@param opts SpawnOpts
+---@return boolean
 local function spawn(opts)
   ---@type uv
   local uv = require 'luv'
-  local output_data = {}
-  local output_error = {}
-  local code = nil
-  local signal = nil
+  ---@type ListenerCallback
+  local on_stdout
+  ---@type ListenerCallback
+  local on_stderr
+  local on_exit
+
+  if opts.listeners ~= nil then
+    on_stdout = opts.listeners.on_stdout
+    on_stderr = opts.listeners.on_stderr
+    on_exit = opts.listeners.on_exit
+  end
+
+  if on_stdout == nil then
+    on_stdout = 'parent'
+  end
+
+  if on_stderr == nil then
+    on_stderr = 'parent'
+  end
 
   ---Spawn a child process
   local stdout
-  if opts.inherit_stdout then
+  if on_stdout == 'parent' then
     stdout = 1
   else
     stdout = uv.new_pipe()
   end
   local stderr
-  if opts.inherit_stderr then
+  if on_stderr == 'parent' then
     stderr = 2
   else
     stderr = uv.new_pipe()
   end
+
   local proc_handle, pid = uv.spawn(
     opts.cmd,
     ---@diagnostic disable-next-line
     { args = opts.args, stdio = { nil, stdout, stderr } },
-
-    ---Process on exit callback
-    function(c, s)
-      code = c
-      signal = s
-    end
+    ---@diagnostic disable-next-line
+    on_exit
   )
 
   ---Process stdout listener
-  if not opts.inherit_stdout then
+  if on_stdout ~= 'parent' then
     ---@diagnostic disable-next-line
-    uv.read_start(stdout, function(err, data)
-      assert(not err, err)
-      table.insert(output_data, data)
-    end)
+    uv.read_start(stdout, on_stdout)
   end
 
   ---Process stderr listener
-  if not opts.inherit_stderr then
+  if on_stderr ~= 'parent' then
     ---@diagnostic disable-next-line
-    uv.read_start(stderr, function(err, data)
-      assert(not err, err)
-      table.insert(output_error, data)
-    end)
+    uv.read_start(stderr, on_stderr)
   end
 
-  uv.run()
-  return code, signal, output_data, output_error
+  return uv.run()
 end
 
----@class ProcessOpts:SpawnOpts
----@field to_json? boolean The resulting output data is converted to json
----@field to_string? boolean The resulting output data is converted to string
-
 ---@class Process
----@field exec fun(opts: ProcessOpts): string|table Execute the executable
+---@field exec fun(opts: SpawnOpts): string|table Execute the executable
 local M = {
-  ---@param opts ProcessOpts
-  ---@return string|table # The executable output data
+  ---@param opts SpawnOpts
+  ---@return boolean
   exec = function(opts)
-    local code, signal, data, err = spawn(opts)
-    if opts.to_json then
-      return json.decode(table.concat(data))
-    elseif opts.to_string then
-      local trimmed = string.gsub(table.concat(data), '%s+', '')
-      return trimmed
-    else
-      return data
-    end
+    return spawn(opts)
   end,
 }
 
