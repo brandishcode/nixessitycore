ca9ee5f0c49ff108dc8558c40d58f335119cacba
brandishcode <brandishcode@gmail.com>
2025-07-06 16:44:00 +0900

    make test of impure flake more explicit

--- spec/flake_packages_spec.lua
+++ spec/flake_packages_spec.lua
@@ -1,15 +1,24 @@
 local nc = require 'nixessitycore'
 
 describe('flake_packages', function()
-  it('should throw error if used on unlocked flake reference', function()
-    local _, err_output = nc.flake_packages('./spec/flakes/single-flake', {
-      is_relative_path = true,
-      debug_mode = 'store',
-    })
-    assert.truthy(string.find(
-      table.concat(err_output),
-      "error: cannot call 'getFlake' on unlocked flake reference" --nix returned error message
-    ) > 0)
+  describe('calling impure flake without impure flag', function()
+    it('should throw error', function()
+      assert.has.errors(function()
+        nc.flake_packages('./spec/flakes/single-flake', {
+          is_relative_path = true,
+        })
+      end)
+    end)
+    it('should throw expected error message', function()
+      local _, err_output = nc.flake_packages('./spec/flakes/single-flake', {
+        is_relative_path = true,
+        debug_mode = 'store',
+      })
+      assert.truthy(string.find(
+        table.concat(err_output),
+        "error: cannot call 'getFlake' on unlocked flake reference" --nix returned error message
+      ) > 0)
+    end)
   end)
 
   it('should return available packages of a flake in impure mode', function()
